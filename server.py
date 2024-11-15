d9547f2aa953a82cfec01689ef142695ab954bac
saidgc <said.g@hotmail.com>
2024-11-14 18:20:11 -0600

    perf: first server attempt

--- server.py
+++ server.py
@@ -1,2 +1,16 @@
 # -*- coding: utf-8 -*-
 # Created by Said at 14/11/24
+
+from fastapi import FastAPI
+
+from common.models.message import Message
+
+server = FastAPI()
+
+
+@server.post('/message')
+async def send_message(message: Message):
+    content = message.content.strip()
+    if content.upper() == "DESCONEXION":
+        return {"response": "Connection closed"}
+    return {"response": content.upper()}
