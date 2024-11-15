c07d058eaa2bfad4f0281283c2482d83fa8b8de3
saidgc <said.g@hotmail.com>
2024-11-14 18:41:38 -0600

    feat: start uvicorn server from server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Created by Said at 14/11/24
 
+import uvicorn
 from fastapi import FastAPI
 
 from common.connection_states import CLOSE_CONNECTION
@@ -15,3 +16,9 @@ async def send_message(message: Message):
     if content.upper() == CLOSE_CONNECTION:
         return {'response': 'Connection closed'}
     return {'response': content.upper()}
+
+
+if __name__ == '__main__':
+    config = uvicorn.Config('server:server', port=5000, log_level='info')
+    server = uvicorn.Server(config)
+    server.run()
