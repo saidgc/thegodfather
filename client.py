c32509bb0161d2c9b39ec42cd7bd115a58015aa0
saidgc <said.g@hotmail.com>
2024-11-14 19:22:57 -0600

    perf: create client

--- client.py
+++ client.py
@@ -1,2 +1,28 @@
 # -*- coding: utf-8 -*-
 # Created by Said at 14/11/24
+
+import asyncio
+
+import websockets
+
+SERVER = 'ws://127.0.0.1:5000/'
+
+
+async def start_client():
+    async with websockets.connect(SERVER) as websocket:
+        print('Connected to server.')
+
+        while True:
+            message = input('Enter message for server: ')
+            await websocket.send(message)
+
+            response = await websocket.recv()
+            print(f'Server response: {response}')
+
+            if response == 'DESCONEXION':
+                print('Server has closed the connection.')
+                break
+
+
+if __name__ == '__main__':
+    asyncio.run(start_client())
