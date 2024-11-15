31f9a484af97495ec0e9909a9c41e31733d01298
saidgc <said.g@hotmail.com>
2024-11-14 18:58:34 -0600

    feat: install websockets

--- server.py
+++ server.py
@@ -2,23 +2,37 @@
 # Created by Said at 14/11/24
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 from common.connection_states import CLOSE_CONNECTION
-from common.models.message import Message
 
 server = FastAPI()
 
 
-@server.post('/')
-async def send_message(message: Message):
-    content = message.content.strip()
-    if content.upper() == CLOSE_CONNECTION:
-        return {'response': 'Connection closed'}
-    return {'response': content.upper()}
+@server.websocket('/')
+async def send_message(web_socket: WebSocket):
+    await web_socket.accept()
+    try:
+        while True:
+            message = await web_socket.receive_text()
+            message = message.upper().strip()
+
+            if message == CLOSE_CONNECTION:
+                await web_socket.send_text('Connection closed')
+                break
+
+            await web_socket.send_text(message)
+
+    except WebSocketDisconnect:
+        await web_socket.close()
 
 
 if __name__ == '__main__':
-    config = uvicorn.Config('server:server', port=5000, log_level='info')
+    config = uvicorn.Config(
+        app='server:server',
+        host='127.0.0.1',
+        port=5000,
+        log_level='info',
+    )
     server = uvicorn.Server(config)
     server.run()
